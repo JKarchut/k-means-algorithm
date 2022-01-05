54ae4107e5e5845a9b6524c7edd8bd28d04cdd56
JKarchut <jkarchut00@gmail.com>
2022-01-05 15:38:09 +0100

    ii

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 make
-./k-means1 -i test.txt -N 10000 -n 3 -k 10
+make -C ./parallel-kmeans/parallel-kmeans seq
+./k-means1 -i test.txt -N 1000000 -n 10 -k 20
+parallel-kmeans/parallel-kmeans/seq_main -i test.txt -o -n 20
