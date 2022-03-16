fd6e5f67da2572309d2e4ba751169da749bc5fcb
Jan Karchut <jkarchut00@gmail.com>
2022-03-16 20:18:54 +0100

    delete files that was required for comparison, add readme

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 make
-make -C ./parallel-kmeans/parallel-kmeans seq
 echo "" >> log.txt
 echo "--- TEST ---" >> log.txt
 echo "dataset 1M vectors of 20 dimensions" >> log.txt
@@ -10,7 +9,3 @@ echo "GPU 1"
 echo "--- GPU 2 ---" >> log.txt
 echo "GPU 2"
 ./k-means2 -i test.txt -N 1000000 -n 10 -k 20 >> log.txt
-
-echo "--- CPU ---" >> log.txt
-echo "CPU"
-parallel-kmeans/parallel-kmeans/seq_main -i test.txt -o -n 20 >> log.txt
